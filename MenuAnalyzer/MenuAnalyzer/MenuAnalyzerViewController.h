fef75c1e37ba6fb9702251b58f11342cbd1dcf92
Jingjie Zheng <i@tsenmu.com>
2016-11-24 16:54:47 -0500

    Added a popover when clicking the menubar item

---
+++ MenuAnalyzer/MenuAnalyzer/MenuAnalyzerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuAnalyzerViewController.h
+//  MenuAnalyzer
+//
+//  Created by Jingjie Zheng on 2016-11-23.
+//  Copyright © 2016 Jingjie Zheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MenuAnalyzerViewController : NSViewController
+
+@end
